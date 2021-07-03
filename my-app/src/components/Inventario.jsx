1027b122e519c336f56734ff373db72d0730b35f
Ignacio Amatt <ignacio_amatt@hotmail.com>
2021-07-03 17:51:16 -0300

    .

--- my-app/src/components/Inventario.jsx
+++ my-app/src/components/Inventario.jsx
@@ -3,23 +3,18 @@ import { Link } from 'react-router-dom';
 
 import styles from './Inventario.module.css';
 
-export default function Inventario({ inventario, setInventario}) {
-
+export default function Inventario({ inventario, setInventario }) {
+ 
   function remove(id) {
-    setInventario(
-      inventario.filter ( pet => pet.id !== id) 
-    )
-  } 
+    setInventario(inventario.filter((pet) => pet.id !== id));
+  }
 
   console.log('Contenido del Inventario:', inventario);
   return (
     <div className={styles.box}>
       <div id={styles.closeDiv}>
         <Link to="/">
-          <button
-            id={styles.btnClose}
-            className="btn btn-danger"
-          >
+          <button id={styles.btnClose} className="btn btn-danger">
             X
           </button>
         </Link>
@@ -29,30 +24,33 @@ export default function Inventario({ inventario, setInventario}) {
         <h2>Inventario</h2>
       </div>
 
-      <div >
-
-        {inventario.length>0 ? 
+      <div>
+        {inventario.length > 0 ? (
           <ul id={styles.pets}>
-          
-            {inventario.map((pet,i) => (
+            {inventario.map((pet, i) => (
               <li key={i}>
-              <img src={pet.img} alt=""></img>
-              <h4>{pet.name}</h4>
-              <button
-              id = {styles.removeBtn}
-              //className="btn btn-danger"
-              onClick={() => remove(pet.id)}><img id={styles.removeImg} src='./img/delete-signv2.png' alt=""></img></button>
+                <img src={pet.img} alt=""></img>
+                <h4>{pet.name}</h4>
+                <button
+                  id={styles.removeBtn}
+                  //className="btn btn-danger"
+                  onClick={() => remove(pet.id)}
+                >
+                  <img
+                    id={styles.removeImg}
+                    src="./img/delete-signv2.png"
+                    alt=""
+                  ></img>
+                </button>
               </li>
             ))}
-
           </ul>
-        : 
+        ) : (
           <div>
-            <br></br>  <br></br>
+            <br></br> <br></br>
             <h4> Aún no tienes mascotas </h4>
           </div>
-        }
-
+        )}
       </div>
     </div>
   );

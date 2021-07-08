17ec22e7c14d42f1e186fb6c4cbe312746496963
Ignacio Amatt <ignacio_amatt@hotmail.com>
2021-07-08 14:18:48 -0300

    modularizando

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,6 +10,7 @@ import NewEgg from './components/NewEgg/NewEgg';
 import NewPet from './components/NewPet/NewPet';
 import Inventario from './components/Inventario/Inventario';
 
+import { Inicial, Comun, Roto, Granja, Azul, Rosa, Safari } from './eggs.js';
 import { Gato, Perro, PerroAzul, GatoRosa } from './pets.js';
 import { Elefante } from './pets/safari.js';
 import {
@@ -34,107 +35,80 @@ const App = () => {
 
   // Establece el tipo de huevo segun random
   function typeOfEgg() {
-    let random = Math.floor(getRandom(1, 101));
+    let random = Math.floor(getRandom());
 
-    if (random > 0 && random <= 30) {
-      setEgg({
-        type: 'common',
-        img: './img/eggs/Huevo_de_mascota.png',
-      });
-    }
-
-    if (random > 30 && random <= 50) {
-      setEgg({
-        type: 'broken',
-        img: './img/eggs/Huevo_roto.png',
-      });
-    }
-
-    if (random > 50 && random <= 70) {
-      setEgg({
-        type: 'farm',
-        img: './img/eggs/Huevo_de_Granja.png',
-      });
-    }
-
-    if (random > 70 && random <= 80) {
-      setEgg({
-        type: 'blue',
-        img: './img/eggs/Huevo_azul.png',
-      });
-    }
-
-    if (random > 80 && random <= 90) {
-      setEgg({
-        type: 'pink',
-        img: './img/eggs/Huevo_rosa.png',
-      });
-    }
-
-    if (random > 90 && random <= 100) {
-      setEgg({
-        type: 'safari',
-        img: './img/eggs/Huevo_de_Safari.png',
-      });
-    }
+    if (random > 0 && random <= 30) setEgg(Comun);
+    if (random > 30 && random <= 50) setEgg(Roto);
+    if (random > 50 && random <= 70) setEgg(Granja);
+    if (random > 70 && random <= 80) setEgg(Azul);
+    if (random > 80 && random <= 90) setEgg(Rosa);
+    if (random > 90 && random <= 100) setEgg(Safari);
 
     console.log(random);
     dispatch(setLayer(0, 'New Egg'));
   }
 
   // Obtiene una nueva mascota segun el huevo
+  // Falta completar. Farm esta listo, ya tiene todos sus pets.
   function newPet() {
-    // Falta terminar de desarrollar. Farm esta listo, ya tiene todos sus pets.
-    let random = getRandom(1, 101);
+    let random = Math.floor(getRandom());
 
-    if (egg.type === 'initial') {
-      if (random > 0 && random <= 50) setPet(Gato); 
-      if (random > 50 && random <= 101) setPet(Perro);
+    switch (egg.type) {
+      case 'initial':
+        if (random > 0 && random <= 50) setPet(Gato);
+        if (random > 50 && random <= 101) setPet(Perro);
+        break;
+      case 'common':
+        setPet(Gato);
+        break;
+      case 'broken':
+        setPet(Perro);
+        break;
+      case 'farm':
+        if (random > 0 && random <= 20) setPet(Pollo); // Comunes 20%
+        if (random > 20 && random <= 37.5) setPet(PatoTonto);
+        if (random > 37.5 && random <= 55) setPet(Pato); // No comunes 35 %
+        if (random > 55 && random <= 68.5) setPet(Cerdo);
+        if (random > 68.5 && random <= 82) setPet(Vaca); // Raros 27%
+        if (random > 82 && random <= 89.5) setPet(Pavo);
+        if (random > 89.5 && random <= 97) setPet(Llama); // Ultra Raros 15%
+        if (random > 97 && random <= 98.5) setPet(Buho);
+        if (random > 98.5 && random <= 100) setPet(Cuervo); // Legendarios 3 %
+        break;
+      case 'blue':
+        setPet(PerroAzul);
+        break;
+      case 'pink':
+        setPet(GatoRosa);
+        break;
+      case 'safari':
+        setPet(Elefante);
+        break;
     }
-
-    if (egg.type === 'common') setPet(Gato);
-
-    if (egg.type === 'broken') setPet(Perro);
-
-    if (egg.type === 'farm') {
-      if (random > 0 && random <= 20) setPet(Pollo); // Comunes 20%
-      if (random > 20 && random <= 37.5) setPet(PatoTonto);
-      if (random > 37.5 && random <= 55) setPet(Pato); // No comunes 35 %
-      if (random > 55 && random <= 68.5) setPet(Cerdo);
-      if (random > 68.5 && random <= 82) setPet(Vaca); // Raros 27%
-      if (random > 82 && random <= 89.5) setPet(Pavo);
-      if (random > 89.5 && random <= 97) setPet(Llama); // Ultra Raros 15%
-      if (random > 97 && random <= 98.5) setPet(Buho);
-      if (random > 98.5 && random <= 100) setPet(Cuervo); // Legendarios 3 %
-    }
-
-    if (egg.type === 'blue') setPet(PerroAzul);
-
-    if (egg.type === 'pink') setPet(GatoRosa);
-
-    if (egg.type === 'safari') setPet(Elefante);
 
     dispatch(setLayer(0, 'New Pet'));
   }
 
+  // Da un huevo de bienvenida de regalo por única vez.
   useEffect(() => {
-    setEgg({
-      type: 'initial',
-      img: './img/eggs/Huevo_Inicial.png',
-    });
-
+    setEgg(Inicial);
     dispatch(setLayer(0, 'New Egg'));
   }, []);
 
   return (
     <div className="App">
-      <Route path="/" render={() => <Nav typeOfEgg={typeOfEgg} />} />
+      
+      <Route path="/">
+        <Nav typeOfEgg={typeOfEgg} />
+      </Route>
 
       <NewEgg newPet={newPet} egg={egg} />
 
       <NewPet pet={pet} setPet={setPet} />
 
-      <Route path="/Inventario" render={() => <Inventario />} />
+      <Route path="/Inventario">
+        <Inventario />
+      </Route>
     </div>
   );
 };
--- my-app/src/eggs.js
+++ my-app/src/eggs.js
@@ -1,11 +1,34 @@
-// sin usar
+export const Inicial = {
+  type: 'initial',
+  img: './img/eggs/Huevo_Inicial.png',
+};
 
+export const Comun = {
+  type: 'common',
+  img: './img/eggs/Huevo_de_mascota.png',
+};
 
-const Eggs = {
-    '1': './img/eggs/Huevo_de_mascota.png', 
-    '2': './img/eggs/Huevo_azul.png',
-    '3': './img/eggs/Huevo_rosa.png',
-}
+export const Roto = {
+  type: 'broken',
+  img: './img/eggs/Huevo_roto.png',
+};
 
+export const Granja = {
+  type: 'farm',
+  img: './img/eggs/Huevo_de_Granja.png',
+};
 
-export default Eggs
+export const Azul = {
+  type: 'blue',
+  img: './img/eggs/Huevo_azul.png',
+};
+
+export const Rosa = {
+  type: 'pink',
+  img: './img/eggs/Huevo_rosa.png',
+};
+
+export const Safari = {
+  type: 'safari',
+  img: './img/eggs/Huevo_de_Safari.png',
+};
